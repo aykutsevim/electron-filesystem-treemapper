cd620e2ab202875bdd43d318ffeb844675824901
aykut sevim <aykutsevim@gmail.com>
2019-03-02 18:48:50 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const electron = require('electron');
 const fs = require('fs');
 const path = require('path');
+var sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database(`${__dirname}/repo.sqlite`);
 
 const { app, BrowserWindow, ipcMain } = electron;
 
@@ -36,6 +39,23 @@ const walkSync = (dir, filelist = []) => {
     return filelist;
 };
 
+ipcMain.on('dbcreate:request', (event) => {
+    db.serialize(function() {
+      db.run("CREATE TABLE lorem (info TEXT)");
+     
+      var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+      for (var i = 0; i < 10; i++) {
+          stmt.run("Ipsum " + i);
+      }
+      stmt.finalize();
+     
+      db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+          console.log(row.id + ": " + row.info);
+      });
+    });
+     
+    db.close();
+});
 
 ipcMain.on('data:request', (event, path) => {
     var chartData = walkSync(path);
